d3a700628a8c30cabe064880f5e850104c6e4481
Vladislav Murzin <vlad99samara@mail.ru>
2022-05-26 13:19:35 +0400

    console app first step

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 "use strict";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
 
-// const result = confirm('Are you here?');
-// console.log(result);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+	  b = prompt('На сколько оцените его?', ''),
+	  c = prompt('Один из последних просмотренных фильмов?', ''),
+	  d = prompt('На сколько оцените его?', '');
 
-// const answer = prompt('Вам есть 18?', '18');
-// console.log(answer);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-
-const answers = [];
-
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваша фамилия?', '');
-answers[2] = prompt('Сколько вам лет?', '');
-
-document.write(answers);
+console.log(personalMovieDB);
